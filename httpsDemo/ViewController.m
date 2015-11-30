ef3798700cc09449aae57ec5ced123cddbcf31ee
hengcj <windfringe@gmail.com>
2015-11-30 17:18:33 +0800

    add gitignore

--- httpsDemo/ViewController.m
+++ httpsDemo/ViewController.m
@@ -3,7 +3,7 @@
 //  httpsDemo
 //
 //  Created by mobile_dev01 on 15/6/26.
-//  Copyright (c) 2015年 sipsd. All rights reserved.
+//  Copyright (c) 2015年 windfringe. All rights reserved.
 //
 
 #import "ViewController.h"
